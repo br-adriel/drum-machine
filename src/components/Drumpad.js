9153e26a5e364d25b2a2fd5b5e9af2337b6befdf
Adriel <adriel.fariasan@gmail.com>
2021-11-28 13:06:19 -0300

    Adiciona componente Drumpad

--- src/components/Drumpad.js
+++ src/components/Drumpad.js
@@ -1,7 +1,11 @@
 import "./Drumpad.css";
 
 const Drumpad = (props) => {
-  return <div className="drumpad"></div>;
+  return (
+    <button id={props.id} className="drum-pad">
+      {props.label}
+    </button>
+  );
 };
 
 export default Drumpad;
