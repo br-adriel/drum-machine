ea3729360f43fe20d7464d77780bdab968dfe42a
Adriel <adriel.fariasan@gmail.com>
2021-11-30 16:34:28 -0300

    Adiciona componente Controls

---
+++ src/components/Controls.js
@@ -0,0 +1,7 @@
+import "./Controls.css";
+
+const Controls = (props) => {
+  return <section className=".controls">{props.children}</section>;
+};
+
+export default Controls;
--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -1,10 +1,12 @@
-import "./DrumMachine.css";
+import Controls from "./Controls";
 import DrumpadArea from "./DrumpadArea";
+import "./DrumMachine.css";
 
 const DrumMachine = (props) => {
   return (
     <div id="drum-machine" className="drum-machine">
       <DrumpadArea />
+      <Controls />
     </div>
   );
 };
