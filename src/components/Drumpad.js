4814d7255415479da887f58539b220109538e119
Adriel <adriel.fariasan@gmail.com>
2021-11-30 11:34:45 -0300

    Implementa os audios da aplicacao

--- src/components/Drumpad.js
+++ src/components/Drumpad.js
@@ -1,10 +1,27 @@
 import "./Drumpad.css";
+import { useEffect } from "react";
 
 const Drumpad = (props) => {
+  const keyCode = props.label.charCodeAt(0);
+  const playAudio = () => {
+    document.getElementById(props.label).play();
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      if (e.key === keyCode) {
+        playAudio();
+      }
+    });
+  }, []);
+
   return (
-    <button id={props.id} className="drum-pad">
-      {props.label}
-    </button>
+    <>
+      <button id={props.id} className="drum-pad" onClick={playAudio}>
+        {props.label}
+        <audio src={props.url} id={props.label} className="clip"></audio>
+      </button>
+    </>
   );
 };
 
--- src/components/DrumpadArea.js
+++ src/components/DrumpadArea.js
@@ -2,17 +2,66 @@ import "./DrumpadArea.css";
 import Drumpad from "./Drumpad";
 
 const DrumpadArea = (props) => {
+  const drumpads = [
+    {
+      id: "Heater-1",
+      label: "Q",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    },
+    {
+      id: "Heater-2",
+      label: "W",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    },
+    {
+      id: "Heater-3",
+      label: "E",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    },
+    {
+      id: "Heater-4_1",
+      label: "A",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    },
+    {
+      id: "Heater-6",
+      label: "S",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    },
+    {
+      id: "Dsc_Oh",
+      label: "D",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    },
+    {
+      id: "Kick_n_Hat",
+      label: "Z",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    },
+    {
+      id: "RP4_KICK_1",
+      label: "X",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    },
+    {
+      id: "Cev_H2",
+      label: "C",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+
   return (
     <div className="drumpad-area">
-      <Drumpad label="Q"></Drumpad>
-      <Drumpad label="W"></Drumpad>
-      <Drumpad label="E"></Drumpad>
-      <Drumpad label="A"></Drumpad>
-      <Drumpad label="S"></Drumpad>
-      <Drumpad label="D"></Drumpad>
-      <Drumpad label="Z"></Drumpad>
-      <Drumpad label="X"></Drumpad>
-      <Drumpad label="C"></Drumpad>
+      {drumpads.map((item) => {
+        return (
+          <Drumpad
+            key={item.label}
+            label={item.label}
+            url={item.url}
+            id={item.id}
+          />
+        );
+      })}
     </div>
   );
 };
